f229efeae7e0b1f70cb94b612d6603d23765b1f4
Silvio Sebastiany <silvio.sebastiany.gmail.com>
2025-11-25 23:04:19 -0300

    limpeza de comentarios

--- src/MetaQuery.Domain/Commands/DesativarMetadado/DesativarMetadadoCommand.cs
+++ src/MetaQuery.Domain/Commands/DesativarMetadado/DesativarMetadadoCommand.cs
@@ -2,14 +2,8 @@ using MediatR;
 
 namespace MetaQuery.Domain.Commands.DesativarMetadado
 {
-    /// <summary>
-    /// Command para desativar (soft delete) um metadado
-    /// </summary>
     public record DesativarMetadadoCommand : IRequest<bool>
-    {
-        /// <summary>
-        /// ID do metadado a ser desativado
-        /// </summary>
+    {   
         public int Id { get; init; }
 
         public DesativarMetadadoCommand(int id)
